98b874e7a4de10de6b9cfa7220d3b1e121e49fe4
B-ASTRA <87750421+B-ASTRA@users.noreply.github.com>
2021-07-21 13:09:42 +0530

    Add files via upload

---
+++ brickgnerator.java
@@ -0,0 +1,64 @@
+package brickbreakergame;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+
+public class MapGenerator
+{
+	public int map[][];
+	
+	public int brickWidth;
+	
+	public int brickHeight;
+	
+	public MapGenerator(int row ,int col) 
+	
+	
+	{
+	    	map = new int[row][col];
+		    for(int i=0;i<map.length;i++) 
+		     {
+			      for(int j=0;j<map[0].length;j++) 
+			          {
+				             map[i][j]=1;
+			          }
+		      }
+		
+		              brickWidth = 550/col;
+		              brickHeight= 140/row;
+	
+	}
+	
+	
+
+		public void draw(Graphics2D g)
+		{
+			for(int i=0;i<map.length;i++)
+			{
+				for(int j=0;j<map[0].length;j++)
+				{
+					if(map[i][j] > 0)
+					{
+						g.setColor(Color.cyan.darker());
+						g.fillRect(j * brickWidth + 75, i * brickHeight + 50,brickWidth,brickHeight);
+						
+						g.setStroke(new BasicStroke(3));
+						g.setColor(Color.black.DARK_GRAY);
+						g.drawRect(j * brickWidth + 75, i * brickHeight + 50,brickWidth,brickHeight);
+					}	
+			   	}
+			}
+		}
+		
+		public void setBrickValue(int value, int row, int col)
+		
+		   {
+			     map[row] [col] =value;
+			}
+		
+}
+		
+		
+
